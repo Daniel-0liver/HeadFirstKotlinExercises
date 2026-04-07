dcd316bdfb50f3ee7b59df686b0bc5dc885dbbf5
Daniel-0liver <dtoliverbr@gmail.com>
2026-04-07 22:25:00 +0100

    Add Canine, Wolf, Hippo, Vet classes and implement animal behaviors

---
+++ Chapter_5/src/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val animals = arrayOf(Hippo(), Wolf())
+    for (animal in animals) {
+        animal.roam()
+        animal.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
